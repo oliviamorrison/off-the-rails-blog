a044c86d56cf3b091d8dea03f51937c2c6afa21a
Sam <samong0105@gmail.com>
2018-08-12 12:01:14 +1200

    DELETE BUTTON YEET

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,9 @@ class PostsController < ApplicationController
     Post.create(title: params[:post][:title], content: params[:post][:content])
     redirect_to(posts_path)
   end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to(posts_path)
+  end
 end
